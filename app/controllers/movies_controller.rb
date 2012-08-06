7cf54e0d591692a5426fa43942c573925e1a768a
skipper913 <skipper913@gmail.com>
2012-08-06 04:40:31 +0000

    Homeword 2 Part 3.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,11 @@ class MoviesController < ApplicationController
     @sorted_by = ""
     if (@sort_headers.include? params[:sort_by]) then
       @sorted_by = params[:sort_by]
+      session[:sort_by] = @sorted_by
+    elsif (@sort_headers.include? session[:sort_by]) then
+      @sorted_by = session[:sort_by]
+      flash.keep
+      redirect_to movies_path(params.merge(:sort_by => @sorted_by)) and return
     end
 
     @all_ratings = Movie.select(:rating).map(&:rating).uniq
@@ -19,7 +24,20 @@ class MoviesController < ApplicationController
       @ratings = params[:ratings].keys.reject do |x|
         !@all_ratings.include? x
       end
+      session[:ratings] = @ratings
+    elsif params[:ratings].is_a?(Array) then
+      @ratings = params[:ratings].reject do |x|
+        !@all_ratings.include? x
+      end
+      session[:ratings] = @ratings
+    elsif session[:ratings].is_a?(Array) then
+      @ratings = session[:ratings].reject do |x|
+        !@all_ratings.include? x
+      end
+      flash.keep
+      redirect_to movies_path(params.merge(:ratings  => @ratings)) and return
     end
+
 
     #@movies = Movie.all
     @movies = Movie.find(:all, :conditions  => ["rating in (?)", @ratings], :order => @sorted_by)
